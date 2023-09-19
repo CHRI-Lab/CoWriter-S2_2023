c1f8dfda47c48d1bb142530880a9feac956fabf9
Shen52-YC <yangchens2@student.unimelb.edu.au>
2023-09-20 05:48:53 +1000

    OCR working on Linux

--- ssim_similarity.py
+++ ssim_similarity.py
@@ -16,20 +16,20 @@ image_pathR = '/home/shen/letter/NA-Redback/cat-a.jpg' # robot writing
 child_image = cv2.imread(image_pathC, cv2.IMREAD_GRAYSCALE)
 robot_image = cv2.imread(image_pathR, cv2.IMREAD_GRAYSCALE)
 
-# # OCR
-# pytesseract.pytesseract.tesseract_cmd = r'/home/shen/letter/NA-Redback/tesseract/tesseract.exe'
-# # Load the image with handwritten text
-# img = Image.open(image_pathC)
-# # Perform OCR using Tesseract
-# custom_config = r'--oem 3 --psm 6'
-# text = pytesseract.image_to_string(img,lang='eng', config=custom_config)
-# text.replace(' ', '')
-# print(text)
-# total_characters = len(text)
-# correct_characters = sum(1 for i, j in zip(text, input_text) if i == j)
-# ocr = (correct_characters / total_characters)
-# # Print the result
-# print(f"Letter Binary: {ocr:.2f}")
+# OCR
+# pytesseract.pytesseract.tesseract_cmd = r'/home/shen/letter/NA-Redback/tesseract'
+# Load the image with handwritten text
+img = Image.open(image_pathC)
+# Perform OCR using Tesseract
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(img,lang='eng', config=custom_config)
+text.replace(' ', '')
+print(text)
+total_characters = len(text)
+correct_characters = sum(1 for i, j in zip(text, input_text) if i == j)
+ocr = (correct_characters / total_characters)
+# Print the result
+print(f"Letter Binary: {ocr:.2f}")
 
 # Ensure image has same shape
 if child_image.shape != robot_image.shape:
@@ -46,7 +46,7 @@ hamming_distance = np.count_nonzero(child_image != robot_image)/20000
 print(f"Hamming Distance：{hamming_distance}")
 
 letterAcc = -1*mse*weight[0] + ssim*weight[1] - hamming_distance*weight[2]
-print (letterAcc)
+print ("Overall_Acc", letterAcc)
 
 # f= canvas.Canvas("feedback.pdf")
 # def feedback(f):
@@ -57,7 +57,7 @@ print (letterAcc)
 #     f.showPage()
 #     f.save()
 
-if letterAcc<0.3:
+if letterAcc<0.4:
     print("Your hand wrting letter:",input_text, "is not well, and it looks like:",text)
 
 
