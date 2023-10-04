9beeaa25e8d799c7a1ce461e8b7e2ff5ae63849c
Chien-Pu Lin <chienpu.lin@student.unimelb.edu.au>
2023-10-04 23:16:30 +1100

    update interactive_learning

--- src/bluering_letter_learning/bluering_letter_learning/interactive_learning.py
+++ src/bluering_letter_learning/bluering_letter_learning/interactive_learning.py
@@ -8,53 +8,58 @@ receives interaction inputs e.g. which words to write and user demonstrations,
 passes these demonstrations to the learning algorithm, and publishes the 
 resulting learned shapes for the robot and tablet to draw.
 """
-import rospy
+import rclpy
+from rclpy.node import Node
+from rclpy.duration import Duration
 from geometry_msgs.msg import PointStamped
 from std_msgs.msg import String, Empty, Bool, Float64MultiArray
 from nav_msgs.msg import Path
-from bluering_letter_learning.msg import Shape as ShapeMsg
-from bluering_letter_learning.msg import ActionToDo as ActionToDoMsg
-from bluering_letter_learning.state_machine import StateMachine
-from bluering_letter_learning.controllers.state_controller import (
+from bluering_letter_learning.include.state_machine import StateMachine
+from bluering_letter_learning.include.controllers.state_controller import (
     StateController,
 )
-from bluering_letter_learning.srv import *
+from interface.msg import Shape as ShapeMsg
+from interface.msg import ActionToDo as ActionToDoMsg
+from interface.srv import ClearAllShapes
 
-rospy.init_node("interactive_learning")
-rospy.loginfo("[interactive_learning] Starting...")
 
-
-class InteractiveLearning(object):
+class InteractiveLearning(Node):
     """
     Interaction config parameters come from launch file
     """
 
     def __init__(self):
+        super().__init__("interactive_learning")
+
+        self.get_logger.info("[interactive_learning] Starting...")
+
         # ? Nao parameters
-        self.NAO_IP = rospy.get_param(
+        self.NAO_IP = self.declare_parameter(
             "~nao_ip", "127.0.0.1"
-        )  # ? default behaviour is to connect to simulator locally
-        self.NAO_SPEAKING = rospy.get_param(
+        ).value  # ? default behaviour is to connect to simulator locally
+        self.NAO_SPEAKING = self.declare_parameter(
             "~nao_speaking", True
-        )  # ? whether or not the robot should speak
-        self.NAO_WRITING = rospy.get_param(
+        ).value  # ? whether or not the robot should speak
+        self.NAO_WRITING = self.declare_parameter(
             "~nao_writing", True
-        )  # ? whether or not the robot should move its arms
-        self.NAO_STANDING = rospy.get_param(
+        ).value  # ? whether or not the robot should move its arms
+        self.NAO_STANDING = self.declare_parameter(
             "~nao_standing", True
-        )  # ? whether or not the robot should stand or rest on its knies
-        self.NAO_CONNECTED = rospy.get_param(
+        ).value  # ? whether or not the robot should stand or rest on its knies
+        self.NAO_CONNECTED = self.declare_parameter(
             "~use_robot_in_interaction", True
-        )  # ? whether or not the robot is being used for the interaction (looking, etc.)
+        ).value  # ? whether or not the robot is being used for the interaction (looking, etc.)
         self.NAO_WRITING = (
             self.NAO_WRITING and self.NAO_CONNECTED
         )  # ? use NAO_CONNECTED var as the stronger property
         self.NAO_SPEAKING = self.NAO_SPEAKING and self.NAO_CONNECTED
         self.NAO_STANDING = self.NAO_STANDING and self.NAO_CONNECTED
-        self.LANGUAGE = rospy.get_param(
+        self.LANGUAGE = self.declare_parameter(
             "~language", "english"
-        )  # ? language to speak
-        self.NAO_HANDEDNESS = rospy.get_param("~nao_handedness", "right")
+        ).value  # ? language to speak
+        self.NAO_HANDEDNESS = self.declare_parameter(
+            "~nao_handedness", "right"
+        ).value
         if self.NAO_HANDEDNESS.lower() == "right":
             self.effector = "RArm"
         elif self.NAO_HANDEDNESS.lower() == "left":
@@ -64,66 +69,68 @@ class InteractiveLearning(object):
         self.isFrontInteraction = True
 
         # ? shape params
-        self.FRAME = rospy.get_param(
+        self.FRAME = self.declare_parameter(
             "~writing_surface_frame_id", "writing_surface"
-        )  # ? Frame ID to publish points in
-        self.FEEDBACK_TOPIC = rospy.get_param(
+        ).value  # ? Frame ID to publish points in
+        self.FEEDBACK_TOPIC = self.declare_parameter(
             "~shape_feedback_topic", "shape_feedback"
-        )  # ? Name of topic to receive feedback on
-        BOUNDING_BOXES_TOPIC = rospy.get_param(
+        ).value  # ? Name of topic to receive feedback on
+        BOUNDING_BOXES_TOPIC = self.declare_parameter(
             "~bounding_boxes_topic", "/boxes_to_draw"
-        )  # ? Name of topic to publish bounding boxes of letters to
-        SHAPE_TOPIC = rospy.get_param(
+        ).value  # ? Name of topic to publish bounding boxes of letters to
+        SHAPE_TOPIC = self.declare_parameter(
             "~trajectory_output_topic", "/write_traj"
-        )  # ? Name of topic to publish shapes to
-        SHAPE_TOPIC_DOWNSAMPLED = rospy.get_param(
+        ).value  # ? Name of topic to publish shapes to
+        SHAPE_TOPIC_DOWNSAMPLED = self.declare_parameter(
             "~trajectory_output_nao_topic", "/write_traj_downsampled"
-        )  # ? Name of topic to publish shapes to
+        ).value  # ? Name of topic to publish shapes to
 
-        self.SHAPE_LOGGING_PATH = rospy.get_param(
+        self.SHAPE_LOGGING_PATH = self.declare_parameter(
             "~shape_log", ""
-        )  # ? path to a log file where all learning steps will be stored
-        self.DATASET_DIR = rospy.get_param("~dataset_directory", "default")
+        ).value  # ? path to a log file where all learning steps will be stored
+        self.DATASET_DIR = self.declare_parameter(
+            "~dataset_directory", "default"
+        ).value
         self.loadDataset()
 
         # ? tablet params
-        self.CLEAR_SURFACE_TOPIC = rospy.get_param(
+        self.CLEAR_SURFACE_TOPIC = self.declare_parameter(
             "~clear_writing_surface_topic", "clear_screen"
-        )
-        self.SHAPE_FINISHED_TOPIC = rospy.get_param(
+        ).value
+        self.SHAPE_FINISHED_TOPIC = self.declare_parameter(
             "~shape_writing_finished_topic", "shape_finished"
-        )
+        ).value
         # ? Name of topic to get gestures representing the active shape for demonstration
-        self.GESTURE_TOPIC = rospy.get_param(
+        self.GESTURE_TOPIC = self.declare_parameter(
             "~gesture_info_topic", "gesture_info"
-        )
+        ).value
 
         # ? interaction params
-        self.WORDS_TOPIC = rospy.get_param(
+        self.WORDS_TOPIC = self.declare_parameter(
             "~words_to_write_topic", "words_to_write"
-        )
-        self.PROCESSED_USER_SHAPE_TOPIC = rospy.get_param(
+        ).value
+        self.PROCESSED_USER_SHAPE_TOPIC = self.declare_parameter(
             "~processed_user_shape_topic", "user_shapes_processed"
-        )  # ? Listen for user shapes
-        self.TEST_TOPIC = rospy.get_param(
+        ).value  # ? Listen for user shapes
+        self.TEST_TOPIC = self.declare_parameter(
             "~test_request_topic", "test_learning"
-        )  # ? Listen for when test card has been shown to the robot
-        self.STOP_TOPIC = rospy.get_param(
+        ).value  # ? Listen for when test card has been shown to the robot
+        self.STOP_TOPIC = self.declare_parameter(
             "~stop_request_topic", "stop_learning"
-        )  # ? Listen for when stop card has been shown to the robot
-        self.NEW_CHILD_TOPIC = rospy.get_param(
+        ).value  # ? Listen for when stop card has been shown to the robot
+        self.NEW_CHILD_TOPIC = self.declare_parameter(
             "~new_teacher_topic", "new_child"
-        )  # ? Welcome a new teacher but don't reset learning algorithm's 'memory'
-        self.PERSON_SIDE = rospy.get_param(
+        ).value  # ? Welcome a new teacher but don't reset learning algorithm's 'memory'
+        self.PERSON_SIDE = self.declare_parameter(
             "~person_side", self.NAO_HANDEDNESS.lower()
-        )  # ? side where person is (left/right)
-        self.PUBLISH_STATUS_TOPIC = rospy.get_param(
+        ).value  # ? side where person is (left/right)
+        self.PUBLISH_STATUS_TOPIC = self.declare_parameter(
             "~camera_publishing_status_topic", "camera_publishing_status"
-        )  # ? Controls the camera based on the interaction state (turn it off for writing b/c CPU gets maxed)
+        ).value  # ? Controls the camera based on the interaction state (turn it off for writing b/c CPU gets maxed)
 
-        self.ACTION_TODO_TOPIC = rospy.get_param(
+        self.ACTION_TODO_TOPIC = self.declare_parameter(
             "~action_todo_topic", "action_todo"
-        )
+        ).value
 
         self.alternateSidesLookingAt = False  # ? if true, nao will look to a different side each time. (not super tested)
         # nextSideToLookAt = 'Right'
@@ -135,37 +142,37 @@ class InteractiveLearning(object):
         ]
 
         """ publishers """
-        self.pub_camera_status = rospy.Publisher(
-            self.PUBLISH_STATUS_TOPIC, Bool, queue_size=10
+        self.pub_camera_status = self.create_publisher(
+            Bool, self.PUBLISH_STATUS_TOPIC, 10
         )
-        self.pub_traj = rospy.Publisher(
-            SHAPE_TOPIC, Path, queue_size=10
+        self.pub_traj = self.create_publisher(
+            Path, SHAPE_TOPIC, 10
         )  # ? publish to traj topic for the trajectory_following to write
-        self.pub_bounding_boxes = rospy.Publisher(
-            BOUNDING_BOXES_TOPIC, Float64MultiArray, queue_size=10
+        self.pub_bounding_boxes = self.create_publisher(
+            Float64MultiArray, BOUNDING_BOXES_TOPIC, 10
         )
-        self.pub_traj_downsampled = rospy.Publisher(
-            SHAPE_TOPIC_DOWNSAMPLED, Path, queue_size=10
+        self.pub_traj_downsampled = self.create_publisher(
+            Path, SHAPE_TOPIC_DOWNSAMPLED, 10
         )
-        self.pub_clear = rospy.Publisher(
-            self.CLEAR_SURFACE_TOPIC, Empty, queue_size=10
+        self.pub_clear = self.create_publisher(
+            Empty, self.CLEAR_SURFACE_TOPIC, 10
         )
-        LISTENING_SIGNAL_TOPIC = rospy.get_param(
+        LISTENING_SIGNAL_TOPIC = self.declare_parameter(
             "~listening_signal_topic", "listening_signal"
-        )  # ? Name of topic to publish listening signal to
-        self.pub_listening_signal = rospy.Publisher(
-            LISTENING_SIGNAL_TOPIC, String, queue_size=10
+        ).value  # ? Name of topic to publish listening signal to
+        self.pub_listening_signal = self.create_publisher(
+            String, LISTENING_SIGNAL_TOPIC, 10
         )  # ? take in either `convo` or `mode``
 
         """ tablet watchdog """
         # TODO: Make a ROS server so that *everyone* can access the connection statuses
-        from bluering_letter_learning.watchdog import Watchdog
+        from bluering_letter_learning.include.watchdog import Watchdog
 
         self.tabletWatchdog = Watchdog("watchdog_clear/tablet", 0.4)
         # robotWatchdog = Watchdog('watchdog_clear/robot', 0.8)
 
     def loadDataset(self):
-        rospy.loginfo(f"[ ] DATASET_DIR : {self.DATASET_DIR}")
+        self.get_logger.info(f"[ ] DATASET_DIR : {self.DATASET_DIR}")
         if self.DATASET_DIR.lower() == "default":  # ? use default
             import inspect
             from shape_learning.shape_modeler import ShapeModeler
@@ -177,12 +184,14 @@ class InteractiveLearning(object):
                 installDirectory
                 + "/share/shape_learning/letter_model_datasets/uji_pen_chars2"
             )
-        rospy.loginfo(f"[+] DATASET_DIR : {self.DATASET_DIR}")
+        self.get_logger.info(f"[+] DATASET_DIR : {self.DATASET_DIR}")
 
     def initControllers(self):
-        rospy.loginfo("[interactive_learning] Initializing StateController")
+        self.get_logger.info(
+            "[interactive_learning] Initializing StateController"
+        )
 
-        rospy.loginfo(
+        self.get_logger.info(
             "Nao configuration: writing=%s, speaking=%s (%s), standing=%s, handedness=%s"
             % (
                 self.NAO_WRITING,
@@ -215,7 +224,7 @@ class InteractiveLearning(object):
             self.pub_camera_status,
             self.pub_bounding_boxes,
             self.pub_clear,
-            self.tabletWatchdog,
+            # self.tabletWatchdog,
             self.pub_traj,
             self.pub_traj_downsampled,
             self.pub_listening_signal,
@@ -277,65 +286,76 @@ class InteractiveLearning(object):
     def subscribeEvents(self):
         """listen for events from ROS"""
         # ? listen for a new child signal
-        new_child_subscriber = rospy.Subscriber(
-            self.NEW_CHILD_TOPIC, String, self.controller.onChildReceived
+        self.create_subscription(
+            String, self.NEW_CHILD_TOPIC, self.controller.onChildReceived
         )
 
         # ? listen for words to write
-        words_subscriber = rospy.Subscriber(
-            self.WORDS_TOPIC, String, self.controller.onWordReceived
+        self.create_subscription(
+            String, self.WORDS_TOPIC, self.controller.onWordReceived
         )
 
         # ? listen for request to clear screen (from tablet)
-        clear_subscriber = rospy.Subscriber(
-            self.CLEAR_SURFACE_TOPIC,
+        self.create_subscription(
             Empty,
+            self.CLEAR_SURFACE_TOPIC,
             self.controller.onClearScreenReceived,
         )
 
         # ? listen for test time
-        test_subscriber = rospy.Subscriber(
-            self.TEST_TOPIC, Empty, self.controller.onTestRequestReceived
+        self.create_subscription(
+            Empty, self.TEST_TOPIC, self.controller.onTestRequestReceived
         )
 
         # ? listen for when to stop
-        stop_subscriber = rospy.Subscriber(
-            self.STOP_TOPIC, Empty, self.controller.onStopRequestReceived
+        self.create_subscription(
+            Empty, self.STOP_TOPIC, self.controller.onStopRequestReceived
         )
 
         # ? listen for user-drawn shapes
-        shape_subscriber = rospy.Subscriber(
-            self.PROCESSED_USER_SHAPE_TOPIC,
+        self.create_subscription(
             ShapeMsg,
+            self.PROCESSED_USER_SHAPE_TOPIC,
             self.controller.onUserDrawnShapeReceived,
         )
 
         # ? listen for user-drawn finger gestures
-        gesture_subscriber = rospy.Subscriber(
-            self.GESTURE_TOPIC,
+        self.create_subscription(
             PointStamped,
+            self.GESTURE_TOPIC,
             self.controller.onSetActiveShapeGesture,
         )
 
         # ? wait for finishing signal from where ?
-        self.controller.infoToRestore_waitForShapeToFinish = rospy.Subscriber(
-            self.SHAPE_FINISHED_TOPIC, String, self.controller.onShapeFinished
+        self.controller.infoToRestore_waitForShapeToFinish = (
+            self.create_subscription(
+                String,
+                self.SHAPE_FINISHED_TOPIC,
+                self.controller.onShapeFinished,
+            )
         )
 
         # ? listen for what to act from diagram_manager
-        to_act_subscriber = rospy.Subscriber(
-            self.ACTION_TODO_TOPIC, ActionToDoMsg, self.controller.onToAct
+        self.create_subscription(
+            ActionToDoMsg, self.ACTION_TODO_TOPIC, self.controller.onToAct
         )
 
     def spin(self):
         # ? initialise display manager for shapes (manages positioning of shapes)
-        rospy.loginfo(
+        self.get_logger.info(
             "Waiting for display manager services to become available"
         )
-        rospy.wait_for_service("clear_all_shapes")
 
-        rospy.sleep(
-            2.0
+        clear_all_shapes = self.create_client(
+            ClearAllShapes, "clear_all_shapes"
+        )
+        while not clear_all_shapes.wait_for_service(timeout_sec=1.0):
+            self.get_logger.info(
+                "Display manager services not available, waiting..."
+            )
+
+        self.get_clock().sleep_for(
+            Duration(seconds=2.0)
         )  # ? Allow some time for the subscribers to do their thing,
         # ? or the first message will be missed (eg. first traj on tablet, first clear request locally)
 
@@ -348,15 +368,21 @@ class InteractiveLearning(object):
             self.infoForStartState
         )  # ? run the program with the initial state
 
-        rospy.spin()
+        rclpy.spin(self)
 
         self.tabletWatchdog.stop()
         # robotWatchdog.stop()
 
 
-if __name__ == "__main__":
-    rospy.loginfo("[interactive_learning] Entering main")
+def main(args=None):
+    rclpy.init(args=args)
     iLearning = InteractiveLearning()
     iLearning.initControllers()
     iLearning.subscribeEvents()
     iLearning.spin()
+    iLearning.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
