1e9cba71fb8e2f120cd2fac096995944d35cc4ae
Aurélien <aplaire@student.unimelb.edu.au>
2023-10-08 12:03:24 +1100

    add redback frontend

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CanvasManager from "./components/CanvasManager";
+
+
+function App() {
+    return (
+        <div className="App">
+            <CanvasManager />
+        </div>
+    );
+}
+
+export default App
