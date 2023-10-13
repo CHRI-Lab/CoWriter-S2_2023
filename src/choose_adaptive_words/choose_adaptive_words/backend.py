d86ea2030b4de99870f5bc9f49e5ac36b0499d93
Aurelien <aplaire@student.unimelb.edu.au>
2023-10-13 01:16:44 +0000

    restore child UI waiting Difan

--- src/choose_adaptive_words/choose_adaptive_words/backend.py
+++ src/choose_adaptive_words/choose_adaptive_words/backend.py
@@ -9,31 +9,31 @@ from .app import create_app
 
 TOPIC_WORDS_TO_WRITE = "words_to_write"
 
-
+# TODO @Difan
 def main():
     rclpy.init()
 
     manager_bridge = ManagerUIBridge()
-    child_bridge = ChildUIBridge()
+    #child_bridge = ChildUIBridge()
 
     executor = MultiThreadedExecutor()
     executor.add_node(manager_bridge)
-    executor.add_node(child_bridge)
+    #executor.add_node(child_bridge)
 
     thread = Thread(target=executor.spin)
     thread.start()
 
     app = create_app()
     app.manager_bridge = manager_bridge
-    app.child_bridge = child_bridge
+    #app.child_bridge = child_bridge
 
     try:
         app.run(host="0.0.0.0", port=3001, debug=True)
     finally:
         manager_bridge.get_logger().info("Shutting down ROS2 Node . . .")
         manager_bridge.destroy_node()
-        child_bridge.get_logger().info("Shutting down ROS2 Node . . .")
-        child_bridge.destroy_node()
+        #child_bridge.get_logger().info("Shutting down ROS2 Node . . .")
+        #child_bridge.destroy_node()
         executor.shutdown()
 
 
